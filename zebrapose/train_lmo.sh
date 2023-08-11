738106a1ddfd0cb2b33860035bb3d65ecf42f1cd
Peter Hoenig <hpfreak1004@gmail.com>
2023-08-11 18:10:54 +0200

    update

---
+++ zebrapose/train_lmo.sh
@@ -0,0 +1,8 @@
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name ape
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name can
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name cat
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name driller
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name duck
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name eggbox
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name glue
+python train.py --cfg config/config_BOP/lmo/exp_lmo_BOP.txt --obj_name holepuncher
