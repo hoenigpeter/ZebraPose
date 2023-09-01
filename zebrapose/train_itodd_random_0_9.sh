dc20fc05144f68df3e1559cede5f55a9c104b4a4
Peter Hoenig <hpfreak1004@gmail.com>
2023-09-01 11:15:35 +0200

    bugfix

--- zebrapose/train_itodd_random_0_9.sh
+++ zebrapose/train_itodd_random_0_9.sh
@@ -1,3 +1,3 @@
 for i in {1..9}; do
-python train.py --cfg config/config_BOP_effnet/itodd/itodd_BOP_effnet_train.txt --obj_name obj0$i
+python train.py --cfg config/config_BOP_effnet/itodd/itodd_random_BOP_effnet_train.txt --obj_name obj0$i
 done
