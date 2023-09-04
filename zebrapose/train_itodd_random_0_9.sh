9afe0c58fb57c27c875ce103013d6cb5906b6e4d
Peter Hoenig <hpfreak1004@gmail.com>
2023-09-04 14:55:58 +0200

    update

--- zebrapose/train_itodd_random_0_9.sh
+++ zebrapose/train_itodd_random_0_9.sh
@@ -1,3 +1,3 @@
 for i in {1..9}; do
-python train.py --cfg config/config_BOP/itodd/itodd_random_BOP_train.txt --obj_name obj0$i
+python train.py --cfg config/config_BOP/itodd/itodd_random_BOP_train.txt --obj_name obj0$i --sym_aware_training True
 done
